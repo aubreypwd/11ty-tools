0e79624d25317ffd2735c7534c35175972ab1ca1
Aubrey Portwood <code@aubreypwd.com>
2026-02-18 11:22:02 -0700

    This seems to be working.

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-const path = require("path");
+module.exports = function (eleventyConfig, options = {}) {
 
-module.exports = function (eleventyConfig) {
+  eleventyConfig.addShortcode("test", () => {
+    return `This is a test.`;
+  });
 
-  const toolsEntry = require.resolve("@aubreypwd/11ty-tools");
-  const toolsDir = path.dirname(toolsEntry);
-
-  // 🔥 1️⃣ Watch the entire tools package
-  eleventyConfig.addWatchTarget(toolsDir);
-
-  // 🔥 2️⃣ Clear Node require cache EVERY rebuild
-  delete require.cache[toolsEntry];
-
-  // 🔥 3️⃣ Load plugin fresh
-  eleventyConfig.addPlugin(require("@aubreypwd/11ty-tools"));
-
-  return {
-    dir: {
-      input: "src",
-      includes: "_includes",
-      output: "docs"
-    },
-
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
-
-    // 🔥 4️⃣ Disable Nunjucks caching
-    nunjucksEnvironmentOptions: {
-      noCache: true
-    }
-  };
 };
