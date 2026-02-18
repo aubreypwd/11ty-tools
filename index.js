e917ec308f4defd266377a55f54147e84def555d
Aubrey Portwood <code@aubreypwd.com>
2026-02-18 12:23:54 -0700

    Externalize the test shortcode.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const path = require( 'path' );
 const markdownIt = require( 'markdown-it' );
 
-module.exports = function (eleventyConfig, options = {}) {
+module.exports = function ( eleventyConfig, options = {} ) {
 
 	// Add a markdown filter.
 	eleventyConfig.addFilter( 'markdown', content => new markdownIt( { html: true } ).render( String( content ) ) );
 
 	// Refresh anytime things change...
-  eleventyConfig.addWatchTarget( __dirname );
-  eleventyConfig.on( 'eleventy.before', () => delete require.cache[ require.resolve( __filename ) ] );
+	eleventyConfig.addWatchTarget( __dirname );
+	eleventyConfig.on( 'eleventy.before', () => delete require.cache[ require.resolve( __filename ) ] );
 
-  // Test shortcode.
-	eleventyConfig.addShortcode( 'test', () =>  'This is a test 3.' );
+	// Shortcodes.
+	eleventyConfig.addShortcode( 'test', require( './shortcodes/test.js' ) );
 };
