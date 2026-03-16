94073cb0452d89654bd568303004bbef7f9974a8
Aubrey Portwood <code@aubreypwd.com>
2026-03-15 19:21:19 -0600

    Add streetAddress

--- data/jsonld.js
+++ data/jsonld.js
@@ -52,13 +52,14 @@ module.exports = {
 	LocalBusiness: {
 		'@type': 'LocalBusiness',
 		'@id': `${ site.baseUrl }#LocalBusiness`,
-		image: site.meta?.img ? `${ site.baseUrl }${ site.meta.img }` : '',
+		image: site.meta?.img ? `${ site.baseUrl }/${ site.meta.img }` : '',
 		name: site.title ?? '',
 		alternateName: site.meta?.author ?? '',
 		description: site.meta?.desc ?? '',
 		url: `${ site.baseUrl }/`,
 		telephone: '',
 		priceRange: '',
+		streetAddress: '',
 		areaServed: '',
 		sameAs: [],
 		address: {
