11df9fcb0fdf4cded57524ba43850441a840a32b
Aubrey Portwood <code@aubreypwd.com>
2026-03-17 17:40:27 -0600

    Fix readfile so it can work without a base.

--- shortcodes/readfile.js
+++ shortcodes/readfile.js
@@ -1,3 +1,11 @@
 const required = require( '@root/required.js' );
 
-module.exports = ( eleventyConfig ) => eleventyConfig.addShortcode( 'readfile', ( base = eleventyConfig.dir.output, file ) => required.fs.readFileSync( required.path.resolve( base, file ), 'utf8' ) );
+module.exports = ( eleventyConfig ) => eleventyConfig.addShortcode( 'readfile', ( base = eleventyConfig.dir.output, file ) => {
+
+	if ( 'undefined' === typeof file ) {
+		file = base;
+		base = eleventyConfig.dir.output;
+	}
+
+	return required.fs.readFileSync( required.path.resolve( base, file ), 'utf8' );
+} );
