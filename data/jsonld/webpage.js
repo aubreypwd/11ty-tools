0bb39060efeda2706fbcdb54bb498af2b46bed65
Aubrey Portwood <code@aubreypwd.com>
2026-03-24 16:13:27 -0600

    Remove trailing slash.

--- data/jsonld/webpage.js
+++ data/jsonld/webpage.js
@@ -6,7 +6,7 @@ module.exports = ( site, overrides = {} ) => {
 			'@type': 'WebPage',
 			'@id': `${ site.baseUrl }#WebPage`,
 			name: '',
-			url: `${ site.baseUrl }/#WebPage`,
+			url: `${ site.baseUrl }#WebPage`,
 			description: '',
 			inLanguage: site.lang ?? '',
 			isPartOf: { '@id': `${ site.baseUrl }#WebSite` },
